c7fd9fb221793b44f8ad0e0042b08733382f5cb0
Matt Ellis <mellis@ncees.org>
2019-01-15 14:31:11 -0500

    Order by street address #16

--- app/controllers/manage/people_controller.rb
+++ app/controllers/manage/people_controller.rb
@@ -2,6 +2,6 @@ class Manage::PeopleController < ApplicationController
     before_action :logged_in?
 
     def index
-        @people = Person.all
+        @people = Person.all.order('street_number')
     end
 end
