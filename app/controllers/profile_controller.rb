fbddf188bbdca62f0b134df36be6477b211378ca
Matt Ellis <mellis@ncees.org>
2019-02-02 00:32:10 -0500

    Show dues paid/owed on profile #7

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,60 +1,62 @@
 class ProfileController < ApplicationController
-    before_action :logged_in?
+  before_action :logged_in?
 
-    def index
+  def index
+    @year = Date.current.strftime('%Y')
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    @person.street_number = @user.street_number
+
+    if @person.save
+      flash[:success] = 'Person added'
+      redirect_to profile_residents_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      flash[:success] = 'Person saved'
+
+      redirect_to profile_index_path
+    else
+      render 'edit'
+    end
+  end
+
+  def residents
+    @people = Person.where(street_number: @user.street_number)
+  end
+
+  def resident_delete
+    @person = Person.find(params[:id])
+
+    if !@person.password_digest.nil? || @person.street_number != @user.street_number
+      flash[:danger] = 'Permission denied'
+    else
+      flash[:info] = "Removed #{@person}"
+      @person.destroy
     end
 
-    def new
-        @person = Person.new
-    end
+    redirect_to profile_residents_path
+  end
 
-    def create
-        @person = Person.new(person_params)
-        @person.street_number = @user.street_number
+  private
 
-        if @person.save
-            flash[:success] = 'Person added'
-            redirect_to profile_residents_path
-        else
-            render 'new'
-        end
-    end
-
-    def edit
-        @person = Person.find(params[:id])
-    end
-
-    def update
-        @person = Person.find(params[:id])
-
-        if @person.update(person_params)
-            flash[:success] = 'Person saved'
-
-            redirect_to profile_index_path
-        else
-            render 'edit'
-        end
-    end
-
-    def residents
-        @people = Person.where(street_number: @user.street_number)
-    end
-
-    def resident_delete
-        @person = Person.find(params[:id])
-
-        if @person.password_digest != nil || @person.street_number != @user.street_number
-            flash[:danger] = 'Permission denied'
-        else
-            flash[:info] = "Removed #{@person}"
-            @person.destroy
-        end
-
-        redirect_to profile_residents_path
-    end
-
-    private
-        def person_params
-            params.require(:person).permit(:first_name, :last_name, :email, :phone)
-        end
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :email, :phone)
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -13,6 +13,22 @@ class Person < ApplicationRecord
     active && email_verified_at && verified_at
   end
 
+  def amount_due_for_year(year)
+    Due.find_by year: year
+  end
+
+  def paid_yearly_dues?(year)
+    dues = DuesHouse.find_by due: amount_due_for_year(year), house: house
+
+    dues.paid
+  end
+
+  def dues_for_year(year)
+    return "$#{format('%0.02f', amount_due_for_year(year).amount)} owed" unless paid_yearly_dues?(year)
+
+    "#{year} dues paid"
+  end
+
   has_secure_password validations: false
 
   validates :first_name, presence: true
